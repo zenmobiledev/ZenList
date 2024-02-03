320f523bfcce68231fa4c8a987171fb2e1c5541d
Zaenal Arif <zaenalariftech@gmail.com>
2024-02-04 05:22:31 +0700

    Initial Commit

---
+++ ZenList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZenList
+//
+//  Created by Zaenal Arif on 04/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
